54cc21ea00bb6151f88c759f677c393335e9262d
Marcos Serpa <marcosserpa@gmail.com>
2016-06-02 16:05:11 -0300

    Removes unused test files Adds the actions to respond to application site Fixes views

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,74 +1,101 @@
 class HotelsController < ApplicationController
-  before_action :set_hotel, only: [:show, :edit, :update, :destroy]
+  include HTTParty
 
-  # GET /hotels
-  # GET /hotels.json
+  base_uri 'murmuring-citadel-60488.herokuapp.com'
+
   def index
-    @hotels = Hotel.all
+    response = if params[:search]
+      self.class.get('/search', { body: { words: params[:search] } })
+    else
+      self.class.get('/')
+    end
+
+    @hotels = if response.code == 404
+      []
+    else
+      JSON.parse(response.body, object_class: Hotel)
+    end
+
+    session[:hotels] = @hotels if params[:search].blank?
+
+    @hotels
   end
 
-  # GET /hotels/1
-  # GET /hotels/1.json
   def show
+    hash = session[:hotels].find{ |hotel| hotel['id'].to_s == params[:id] }
+    @hotel = OpenStruct.new(id: hash['id'], name: hash['name'], address: hash['address'], star_rating: hash['star_rating'], accomodation_type: hash['accomodation_type'])
   end
 
-  # GET /hotels/new
   def new
     @hotel = Hotel.new
   end
 
-  # GET /hotels/1/edit
   def edit
+    hash = session[:hotels].find{ |hotel| hotel['id'].to_s == params[:id] }
+    @hotel = Hotel.new(name: hash['name'], address: hash['address'], star_rating: hash['star_rating'], accomodation_type: hash['accomodation_type'])
   end
 
-  # POST /hotels
-  # POST /hotels.json
   def create
-    @hotel = Hotel.new(hotel_params)
+    response = self.class.post('/hotels', { body: hotel_params })
 
     respond_to do |format|
-      if @hotel.save
-        format.html { redirect_to @hotel, notice: 'Hotel was successfully created.' }
-        format.json { render :show, status: :created, location: @hotel }
+      if response.code == 201
+        format.html { redirect_to hotels_url, notice: 'Hotel was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @hotel.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /hotels/1
-  # PATCH/PUT /hotels/1.json
   def update
+    response = self.class.put("/hotels/#{ params['id'] }", { body: hotel_params })
+
     respond_to do |format|
-      if @hotel.update(hotel_params)
+      if response.code == 200
+        @hotel = JSON.parse(response.body, object_class: Hotel)
+
         format.html { redirect_to @hotel, notice: 'Hotel was successfully updated.' }
-        format.json { render :show, status: :ok, location: @hotel }
       else
         format.html { render :edit }
-        format.json { render json: @hotel.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /hotels/1
-  # DELETE /hotels/1.json
   def destroy
-    @hotel.destroy
+    response = self.class.delete("/hotels/#{ params['id'] }")
+
     respond_to do |format|
-      format.html { redirect_to hotels_url, notice: 'Hotel was successfully destroyed.' }
-      format.json { head :no_content }
+      if response.code == 200
+        format.html { redirect_to hotels_url, notice: 'Hotel was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_hotel
-      @hotel = Hotel.find(params[:id])
-    end
+  def search
+    response = self.class.get('/search', { body: params })
 
+    @hotels = JSON.parse(response.body, object_class: Hotel)
+
+    respond_to do |format|
+      if response.code == 201
+        @hotel = OpenStruct.new(hotel_params)
+
+        format.html { redirect_to @hotel, notice: 'Hotel was successfully created.' }
+      end
+    end
+  end
+
+
+  private
     # Never trust parameters from the scary internet, only allow the white list through.
     def hotel_params
       params.require(:hotel).permit(:name, :address, :star_rating, :accomodation_type)
     end
+
+    # def hotel
+    #   hash = session[:hotels].find{ |hotel| hotel['id'].to_s == params[:id] }
+    #   @hotel = OpenStruct.new(name: hash['name'], address: hash['address'], star_rating: hash['star_rating'], accomodation_type: ['accomodation_type'])
+    # end
+
 end
