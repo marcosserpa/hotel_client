f8edb97516874e5c9ba87c185c40f9a9ed861ed4
Marcos Serpa <marcosserpa@gmail.com>
2016-06-02 14:35:44 -0300

    Adds 'httparty' gem to handle HTTP Protocol Adds some tests needed gems

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'httparty'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -35,6 +37,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'pry'
   gem 'rspec-rails'
+  gem 'webmock'
   gem 'awesome_print'
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
--- spec/controllers/hotels_controller_spec.rb
+++ spec/controllers/hotels_controller_spec.rb
@@ -20,22 +20,6 @@ require 'rails_helper'
 
 RSpec.describe HotelsController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Hotel. As you add validations to Hotel, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # HotelsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
   describe "GET #index" do
     it "assigns all hotels as @hotels" do
       hotel = Hotel.create! valid_attributes
